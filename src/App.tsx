071289e1162cea4c62c5acc974133d91ab0495b8
andrew childre <andrewchildre@gmail.com>
2021-07-01 18:44:32 -0600

    switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-const [recipeFound, setRecipeFound] = useState([]);
-const [recipeSearch, setRecipeSearch] = useState('');
-
-const searchForRecipes = async (query: string): Promise<any> => {
-  const result = await fetch(`http://www.recipepuppy.com/api/?search=${query}`);
-  return(await result.json()).result;
-};
 
 
   return (
