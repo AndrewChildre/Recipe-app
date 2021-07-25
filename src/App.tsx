5e9497027665cf6f8c85aef2c4254d6612081126
andrew childre <andrewchildre@gmail.com>
2021-07-25 16:35:49 -0600

    playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import APIfile from './APIfile';
 import './App.css';
-
+import {SandyOne }from './SandyOne';
 
 
 function App() {
-
-
   return (
     <div className="App">
+
   <h1>Hello</h1>
-    <APIfile />
-  
+    {/* <APIfile /> */}
+    <SandyOne/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/SandyOne.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export const SandyOne = ():void => {
+    let names: Array<string> = ['dart', 'frick', 'frinck']
+    return (
+      
+   console.log(names)
+        )};  
+        
